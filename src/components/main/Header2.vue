adaec476e84387c0ebb05b2de6ab5f27492b4da4
Bagus Budi Wibowo <bagusbudi.0701@gmail.com>
2022-10-01 20:28:31 +0700

    update: header2

--- src/components/main/Header2.vue
+++ src/components/main/Header2.vue
@@ -2,7 +2,7 @@
   <div class="bg-white shadow-lg m-6 rounded-l-lg z-50">
 		<div class="flex justify-between items-center mx-6 md:mx-24 py-3">
 			<img src="/images/mdl.png" class="w-[150px] md:w-auto" alt="logo">
-			<router-link :to="$route.meta.nextStep" exact>Skip this step ></router-link>
+			<router-link :to="$route.meta.nextStep" exact>Skip to next step ></router-link>
 		</div>
 	</div>
 </template>
